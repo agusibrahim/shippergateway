4373787a2f21ea6722af78b299b60297688d2ec9
Agus <pymobile123@gmail.com>
2023-01-08 20:33:08 +0700

    fix pickup with timeslots

--- routes/api/pickup/index.dart
+++ routes/api/pickup/index.dart
@@ -6,21 +6,42 @@ import 'package:dio/dio.dart' as d;
 
 Future<Response> onRequest(RequestContext context) async {
   var param = context.request.uri.queryParameters;
-  if (!param.containsKey("order_id") && !param.containsKey("time")) {
-    return Response.json(body: {"status": "ERROR", "msg": "please provide order_id and time pickup"});
+  if (!param.containsKey("order_id")) {
+    return Response.json(body: {"status": "ERROR", "msg": "please provide order_id"});
   }
-  var payload = {
-    "data": {
-      "order_activation": {
-        "order_id": [param['order_id']],
-        "pickup_time": param['time']
+  var payload = {};
+  var hastime = 0;
+  if (param.containsKey("time")) {
+    payload = {
+      "data": {
+        "order_activation": {
+          "order_id": [param['order_id']],
+          "pickup_time": param["time"]
+        }
       }
-    }
-  };
-//  print(json.encode(payload));
-  var r = await httpr.post("/pickup", data: payload, options: d.Options(validateStatus: (status) {
+    };
+    hastime = 1;
+  } else if (param.containsKey("start") && param.containsKey("end")) {
+    payload = {
+      "data": {
+        "order_activation": {
+          "order_id": [param['order_id']],
+          "timezone": "Asia/Jakarta",
+          "start_time": param["start"],
+          "end_time": param["end"]
+        }
+      }
+    };
+    hastime = 2;
+  }
+  if (hastime == 0) {
+    return Response.json(body: {"status": "ERROR", "msg": "please provide time or start/end time"});
+  }
+  print(json.encode(payload));
+  var r =
+      await httpr.post("/pickup${hastime == 2 ? '/timeslot' : ''}", data: payload, options: d.Options(validateStatus: (status) {
     return status! < 500;
   }));
-  //print("Resrp: ${r.data}");
+  print("Resrp: ${r.data}");
   return Response.json(body: {"status": "OK", "msg": "berhasil request pickup", "data": r.data['data']});
 }
